c116678c8b5aa03d827f584f1407bb8955f76bf5
florianvsteen <florian@flownet.be>
2026-02-18 21:14:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def get_data():
 
 if __name__ == '__main__':
     # Listen on all interfaces (0.0.0.0) for container access
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
