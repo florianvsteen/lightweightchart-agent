5a239918eba0290b1a039df4469d172fb1bb36d6
florianvsteen <florian@flownet.be>
2026-02-18 21:03:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def get_data(symbol):
     return jsonify({"candles": chart_data, "markers": markers})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # host='0.0.0.0' is the key for Docker/Container access
+    app.run(debug=True, host='0.0.0.0', port=5000)
