5b9cf19d06e3b625296963dfd11ac3f4490b8fc8
florianvsteen <florian@flownet.be>
2026-02-19 15:44:24 +0100

    Update config.py

--- core/config.py
+++ core/config.py
@@ -4,18 +4,21 @@ ASSET_CONFIGS = {
         "ticker": "YM=F",
         "lookback": 40,
         "threshold": 0.001,
-        "color": "03b2f8"
+        "color": "03b2f8",
+        "port": 500
     },
     "US100": {
         "ticker": "NQ=F",
         "lookback": 45,
         "threshold": 0.0008,
-        "color": "ff9900"
+        "color": "ff9900",
+        "port": 5001
     },
     "XAUUSD": {
         "ticker": "GC=F",
         "lookback": 35,
         "threshold": 0.0015,
-        "color": "ffd700"
+        "color": "ffd700",
+        "port": 5002
     }
 }
